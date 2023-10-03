b832f61ce9b556cbf9329e8cb7f29c276e71a4d8
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-03 12:03:34 +0200

    Opgave 1.3 Færdig

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ console.log(
 console.log("hello world");
 console.log("Opgave 1.2 - Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen");
 console.log("hello  \n world");
+console.log ("Opgave 1.3");
+console.log("*\n **\n ***\n **\n*");
+
 
