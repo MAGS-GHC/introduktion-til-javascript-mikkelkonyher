793cc1da618920e9feb94cef01177322ec20e069
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-03 21:01:09 +0200

    Opgaver 1.4-1.8 færdige

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ console.log("Opgave 1.2 - Lav et program, som skriver “Hello” og på næste 
 console.log("hello  \n world");
 console.log ("Opgave 1.3");
 console.log("*\n **\n ***\n **\n*");
-console.log("opgave 1.4");
-let catName = "mysa"; // string
-
-
+console.log("opgave 1.4 ");
+const catName = "mysa"; // string
+console.log("Opgave 1.5");
+console.log(10-5);
+console.log(25/3);
+console.log(25%3);
+console.log(25%2);
+console.log(3**2);
+console.log("Opgave 1.6");
+let tekst1 = ("hello");
+let tekst2 = ("World!");
+console.log(tekst1+tekst2);
+console.log("opgave 1.7");
+console.log("Opgave 1.8");
+let x = Math.random();
+console.log(x);
