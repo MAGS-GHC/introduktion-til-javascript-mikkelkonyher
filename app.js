57363c691008239b83bce3407c074b8b22cfb939
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-04 11:45:08 +0200

    opgave 1.3-1.9 Løst
    Løs med rettelser og i samarbejde med resten af gruppen

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ console.log("hello  \n world");
 console.log ("Opgave 1.3");
 console.log("*\n **\n ***\n **\n*");
 console.log("opgave 1.4 ");
-const catName = "mysa";
-typeof 'mysa';
+const catNumber = 2
+console.log( typeof catNumber);
 console.log("Opgave 1.5");
 console.log(10-5);
 console.log(25/3);
@@ -19,22 +19,28 @@ console.log(25%3);
 console.log(25%2);
 console.log(3**2);
 console.log("Opgave 1.6");
-let tekst1 = ("hello");
+let tekst1 = ("hello ");
 let tekst2 = ("World!");
 console.log(tekst1+tekst2);
 console.log("opgave 1.7");
-const hold = 'GF2 Online';
+const hold = "GF2 Online";
 console.log(hold.length);
 console.log("Opgave 1.8");
-let x = Math.random();
+let x = Math.floor(Math.random()*100+1)
 console.log(x);
+console.log ("Opgave 1.9");
 let number = 23;
 console.log(number)
 console.log (number = "23")
 console.log(number == "23")
 console.log(number === "23")
-console.log(number += "23")
 console.log(number -= "23")
-console.log(number != "23")
+console.log(number += "23")
+console.log(number += "23")
+console.log(number -= "46")
+console.log(number != 23)
 console.log(number **= 2)
+
+
+
 
