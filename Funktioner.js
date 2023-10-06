a04da48ef780f8e8ba364c8fd0213c7f8b405ce8
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-06 10:59:34 +0200

    Opgave 5.2 færdig

--- Funktioner.js
+++ Funktioner.js
@@ -5,7 +5,19 @@ function myFunction() {
     console.log("Hello World")
    }
 
+console.log ("Opgave 5.2");
 
+function addnumber (a, b) {
+    return a + b;
+}
+
+let sum1 = addnumber (7,8);
+
+let sum2 = addnumber (14,6);
+
+console.log(sum1);
+
+console.log(sum2);
 
 
 
