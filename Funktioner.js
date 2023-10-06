bbe8911200499d12779afd74baec7f62185ee989
mikkelkonyher <146730338+mikkelkonyher@users.noreply.github.com>
2023-10-06 16:46:39 +0200

    Opgave 6.1

--- Funktioner.js
+++ Funktioner.js
@@ -74,3 +74,4 @@ if (erBrugerNavnKorrekt (inputBrugernavn, gemtBrugernavn)) {
 }
 
 
+
---
+++ loop-arrays.js
@@ -0,0 +1,14 @@
+console.log("Opgave 6.1");
+
+const talRække4 = [0, 12, 0, 22, 300, 4, 5]
+
+sum = 0;
+
+for(let i  = 0; i < talRække4.length; i++) {
+    sum += talRække4[i];
+}
+
+console.log("Summen er: " + sum);
+
+console.log ("Opgave 6.2");
+
